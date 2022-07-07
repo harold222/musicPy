2e06513e60431c9ce8a27c79e432ff46c5716934
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-07-07 07:59:30 -0500

    add router and loading

---
+++ frontend/src/components/shared/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+﻿import './loading.css';
+
+export const Loading = () => {
+    return (
+       <>
+        <div className="cssload-container">
+            <div className="cssload-whirlpool"></div>
+        </div>
+       </>
+    )
+}
