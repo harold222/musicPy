ffb92167f2c1b72e71296e9501f5687a44dab9ef
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-07-07 08:47:23 -0500

    adding support to scss

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -1,7 +1,20 @@
-﻿export const Search = () => {
+﻿import { Loading } from "../shared/Loading"
+
+export const Search = () => {
+
     return(
-        <>
-            hola
-        </>
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col">
+                    <h4 className="text-center">
+                        Search music
+                    </h4>
+                    <hr />
+                    <input type="text" />
+
+                    <Loading/>
+                </div>
+            </div>
+        </div>
     )
 }
--- frontend/src/components/shared/Navbar/Navbar.tsx
+++ frontend/src/components/shared/Navbar/Navbar.tsx
@@ -4,18 +4,19 @@ import './Navbar.css';
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link className="navbar-brand" to="/">
-                MusicPy
-            </Link>
+            <div className="container">
+                <Link className="navbar-brand mr-5" to="/">
+                    MusicPy
+                </Link>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-                    <NavLink to="/search"
-                        className={({ isActive }) => "nav-item nav-link" + (isActive ? " active" : "")}
-                    >
-                        Search song
-                    </NavLink>
+                <div className="navbar-collapse">
+                    <div className="navbar-nav">
+                        <NavLink to="/search"
+                            className={({ isActive }) => "nav-item nav-link" + (isActive ? " active" : "")}
+                        >
+                            Search
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </nav>
