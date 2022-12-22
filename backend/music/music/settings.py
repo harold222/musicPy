ff31caaaa94c25aa3e243ec3bae4d1ed86d865e1
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-12-22 15:03:34 -0500

    Max download 1GB

--- backend/music/music/settings.py
+++ backend/music/music/settings.py
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATA_UPLOAD_MAX_MEMORY_SIZE=1000*1024*1024
