d455dd6b29ffc9b3ca18993aa386dd05e756068a
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-12-23 15:17:55 -0500

    style list of songs

--- frontend/src/components/list-song/ListSong.tsx
+++ frontend/src/components/list-song/ListSong.tsx
@@ -1,7 +1,10 @@
 ﻿import ISongsSuggestion from '../../types/ISongsSuggestion';
 import React from 'react';
 import ISearchSong from '../../types/ISearchSong.type';
+import { FaEye, FaPlay } from 'react-icons/fa';
+import { FiPlayCircle } from 'react-icons/fi'
 import './ListSong.scss';
+// import DefaultImage from '../../'
 
 interface ListSongProps {
     songs: ISongsSuggestion[];
@@ -9,20 +12,51 @@ interface ListSongProps {
 
 export const ListSong: React.FC<ListSongProps> = ({ songs }) => {
 
+    const a = (urlSong: string) => {
+        console.log('es. ', import.meta.env)
+    }
+
     return (
-        <>
-            {
-                songs.map((song, idx) =>
-                    <div key={idx} className='list'>
-                        <div className='list-cover'>
-                            <img src={song.thumbnail} alt={song.title}/>
+        <div className='container mt-4'>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    songs.map((song, idx) =>
+                        <div key={idx} className='col list'>
+                            <div className="card h-100 bg-dark">
+                                <img className="card-img-top" src={song.thumbnail} alt={song.title}
+                                    onError={({ currentTarget }) => {
+                                        currentTarget.onerror = null;
+                                        currentTarget.src = "/no-image.png"
+                                    }}/>
+
+                                <div className="card-body list-desc">
+                                    <h5 className="card-title text-center">
+                                        <a className='list-desc-title' href={ song.channel }>
+                                            { song.title }
+                                        </a>
+                                    </h5>
+                                    <div className="row">
+                                        <div className="list-desc-info col-6 text-center">
+                                            <strong>{ song.author }</strong>
+                                            <div className='list-icon'>
+                                                <FaEye size={17} color='white' />
+                                                <span className='list-icon-views'>
+                                                    { song.views.toLocaleString(navigator.language, { maximumFractionDigits: 0 }) }
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="col-6 list-button">
+                                            <button className='list-button-action' onClick={() => a(song.url)}>
+                                                <FiPlayCircle size={50} color='red' />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className='list-desc'>
-                            <h1>{ song.title }</h1>
-                        </div>
-                    </div>
-                )
-            }
-        </>
+                    )
+                }
+            </div>
+        </div>
     );
 };
